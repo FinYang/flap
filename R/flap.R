ad66d5bd2d3c9ae1743d57cd34fcd9bd44f16a10
Fin Yang <yangyangzhuoran@gmail.com>
2024-02-18 18:04:49 +1100

    Doc and rename flap arguments

--- R/flap.R
+++ R/flap.R
@@ -1,9 +1,36 @@
+#' Forecast Linear Augmented Projection
+#'
+#' Reduces forecast variance by adjusting the forecasts of multivariate time
+#' series to be consistent with the forecasts of linear combinations (components)
+#' of the series by projecting all forecasts onto the space where the linear
+#' constraints are satisfied.
+#'
+#' @param fc An \eqn{h} by \eqn{m} matrix of base forecasts of the original
+#' series to be projected. \eqn{h} is the forecast horizon and \eqn{m} is the
+#' total number of series.
+#' @param fc_comp An \eqn{h} by \eqn{p} matrix of base forecasts of the components
+#' used in the projection. \eqn{h} is the forecast horizon and \eqn{p} is the
+#' total number of components.
+#' @param Phi A \eqn{p} by \eqn{m} weight matrix mapping the original series into
+#' the components such that \eqn{c_t = \Phi z_t} where \eqn{c_t} is the vector of
+#' components and \eqn{z_t} is the vector of original series.
+#' @param res A \eqn{T} by \eqn{m} (in-sample) forecast residual matrix of the
+#' original series.
+#' @param res_comp A \eqn{T} by \eqn{p} (in-sample) forecast residual matrix of
+#' the components.
+#' @param p The number of components to use in the projection. The default is
+#' trying all the possible number of components capped at the number provided in
+#' the forecast.
+#'
+#' @return A list of class \code{flap} with each element containing a \eqn{h} by
+#' \eqn{m} matrix of projected forecast of the original series for the corresponding
+#' number of components \code{p}.
 #' @export
-flap <- function(fc_base, fc_comp, Phi, res_base, res_comp,
-                 p = seq_len(ncol(res_comp))) {
-  W <- get_W(res_base, res_comp, p)
+flap <- function(fc, fc_comp, Phi, res, res_comp,
+                 p = seq_len(ncol(fc_comp))) {
+  W <- get_W(res, res_comp, p)
   proj_fc <- project(
-    cbind(fc_base,
+    cbind(fc,
           fc_comp),
     W = W,
     Phi = Phi,
